180a3a506b404460f89830df4ec9792c6857619a
Adedinugra <adedinugra@gmail.com>
2021-10-02 18:12:48 +0700

    Start

---
+++ PraktikumAR/Assets/Script/MenuAR.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAR : MonoBehaviour
+{
+    //Membuat variable untuk resize layar
+    public GUISkin guiSkin;
+    private float guiRatio;
+    private float sWidth;
+    private Vector3 GUIsF;
+
+    public GameObject bumi;
+    public float kecepatanRotasi = 50f;
+    bool statusRotasi = false;
+
+    void Awake()
+    {
+        sWidth = Screen.width;
+        guiRatio = sWidth / 1920;
+        GUIsF = new Vector3(guiRatio, guiRatio, 1);
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin = guiSkin;
+        //letakkan function disini
+        Rotasi();
+
+        void Rotasi()
+        {
+            //meletakkan button di pojok kanan atas
+            GUI.matrix = Matrix4x4.TRS(new Vector3(GUIsF.x, GUIsF.y, 0), Quaternion.identity, GUIsF);
+
+            if (statusRotasi == false)
+            {
+                if (GUI.Button(new Rect(50, 50, 700, 300), "Rotasi"))
+                {
+                    statusRotasi = true;
+                }
+            }
+            else
+            {
+                if (GUI.Button(new Rect(50, 50, 700, 300), "Stop Rotasi"))
+                {
+                    statusRotasi = false;
+                }
+            }
+
+            if (GUI.Button(new Rect(800, 50, 700, 300), "Keluar"))
+            {
+                Application.Quit(); //Keluar dari aplikasi
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (statusRotasi == true)
+        {
+            bumi.transform.Rotate(Vector3.up, kecepatanRotasi * Time.deltaTime);
+        }
+    }
+}
---
+++ PraktikumAR/Assets/Script/VBEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VBEventHandler : MonoBehaviour
+{
+    public GameObject whale;
+    public AudioClip musik;
+    GameObject paus;
+
+    void Start()
+    {
+        whale = GameObject.Find("VirtualButton");
+        whale.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        whale.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+        paus = GameObject.FindWithTag("paus");
+    }
+
+    //button di tekan
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        paus.GetComponent<Animation>().Play();// jika button di tekan maka animasi paus dijalankan
+        GetComponent<AudioSource>().Play();
+        Debug.Log("Button pressed");
+    }
+
+    //button di lepas
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        paus.GetComponent<Animation>().Stop(); //jika button di lepas maka animasi paus akan berhenti
+        GetComponent<AudioSource>().Pause();
+        Debug.Log("Button released");
+    }
+}
