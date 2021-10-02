45538897a8398a54eb06ecca08ca669edbe804e2
Adedinugra <adedinugra@gmail.com>
2021-10-02 18:14:20 +0700

    Start

---
+++ BotanicalMR/Assets/Script/InfoKlik.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoKlik : MonoBehaviour
+{
+
+    public Transform target1;
+    public Texture gambar1, gambar2, gambar3;
+    public string info1, info2, info3;
+    public Vector2 scrollPosition1 = Vector2.zero;
+    public bool show = false;
+    
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform == target1)
+                {
+                    show = true;
+                }
+            }
+        }
+    }
+
+    public void tampil()
+    {
+        show = false;
+    }
+
+    void OnGUI()
+    {
+        if (show == true)
+        {
+            GUI.BeginGroup(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 250, 800, 500));
+            GUI.Box(new Rect(0, 50, 405, 360), "Informasi");
+
+            scrollPosition1 = GUI.BeginScrollView(new Rect(30, 0, 350, 390), scrollPosition1, new Rect(0, 0, 1150, 200));
+
+            GUI.DrawTexture(new Rect(0, 90, 350, 210), gambar1);
+            info1 = GUI.TextArea(new Rect(0, 300, 350, 50), info1, 200);
+
+            GUI.DrawTexture(new Rect(400, 90, 350, 210), gambar2);
+            info2 = GUI.TextArea(new Rect(400, 300, 350, 50), info2, 200);
+
+            GUI.DrawTexture(new Rect(800, 90, 350, 210), gambar3);
+            info3 = GUI.TextArea(new Rect(800, 300, 350, 50), info3, 200);
+
+            GUI.EndScrollView();
+            GUI.EndGroup();
+        }
+    }
+
+}
---
+++ BotanicalMR/Assets/Script/InfoKlikV2.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoKlikV2 : MonoBehaviour
+{
+    public GameObject infoklik;
+    public Image gambar;
+    public Text informasi;
+    public Transform[] target = new Transform[6];
+    public Sprite[] foto = new Sprite[6];
+    public string[] info = new string[6];
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform == target[0])
+                {
+                    informasi.text = info[0];
+                    gambar.sprite = foto[0];
+                }
+                else if (hit.transform == target[1])
+                {
+                    informasi.text = info[1];
+                    gambar.sprite = foto[1];
+                }
+                else if (hit.transform == target[2])
+                {
+                    informasi.text = info[2];
+                    gambar.sprite = foto[2];
+                }
+                else if (hit.transform == target[3])
+                {
+                    informasi.text = info[3];
+                    gambar.sprite = foto[3];
+                }
+                else if (hit.transform == target[4])
+                {
+                    informasi.text = info[4];
+                    gambar.sprite = foto[4];
+                }
+                else if (hit.transform == target[5])
+                {
+                    informasi.text = info[5];
+                    gambar.sprite = foto[5];
+                }
+                infoklik.SetActive(true);
+            }
+        }
+    }
+
+    public void sembunyiInfoKlik()
+    {
+        infoklik.SetActive(false);
+    }
+}
